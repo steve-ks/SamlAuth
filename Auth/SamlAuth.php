0b9ec176d3aa0d6c8f5489d467db21cc6907afde
steve-ks <steve.hegenbart@kingstarter.de>
2018-01-08 15:28:55 +0100

    Bugfix, missing bracketwq

--- Auth/SamlAuth.php
+++ Auth/SamlAuth.php
@@ -139,7 +139,7 @@ class SamlAuth extends Base implements AuthenticationProviderInterface, PreAuthe
                       // Create user by having email as username
                       $this->userInfo = new SamlUserProvider($username, $email, $name, $kbrole);
                       // Check for a redirectAfterLogin value, make sure that the user is redirected to the correct location
-                      if (isset($_POST['RelayState'] && ! filter_var($this->sessionStorage->redirectAfterLogin, FILTER_VALIDATE_URL)
+                      if (isset($_POST['RelayState']) && ! filter_var($this->sessionStorage->redirectAfterLogin, FILTER_VALIDATE_URL)
                             && strpos($_POST['RelayState'],'login') === false)
                           $this->response->redirect($_POST['RelayState']);
                       return true;
